4a3ceee1b7847f545d140a59dbee80c45e59861a
nightsswatch <arthurjihengming@gmail.com>
2021-02-12 22:36:40 +0800

    leetcode

---
+++ src/com/arthur/leetcode/Test.java
@@ -0,0 +1,13 @@
+package com.arthur.leetcode;
+
+/**
+ * @title: Test
+ * @Author ArthurJi
+ * @Date: 2021/2/12 22:34
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Leetcode");
+    }
+}

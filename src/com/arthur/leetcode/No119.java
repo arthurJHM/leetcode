0f14b85246f1f60fd22f967ac0dcc99a01b5e2ec
nightsswatch <arthurjihengming@gmail.com>
2021-02-12 22:55:20 +0800

    leetcode

--- src/com/arthur/leetcode/No119.java
+++ src/com/arthur/leetcode/No119.java
@@ -9,6 +9,6 @@ package com.arthur.leetcode;
 public class No119 {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test2");
+//        System.out.println("test2");
     }
 }

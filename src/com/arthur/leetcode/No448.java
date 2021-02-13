d9a938d248c361733684832b2fc8c12c1badeb2b
nightsswatch <arthurjihengming@gmail.com>
2021-02-13 20:16:05 +0800

    no448补充普通解法

--- src/com/arthur/leetcode/No448.java
+++ src/com/arthur/leetcode/No448.java
@@ -1,6 +1,7 @@
 package com.arthur.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -14,6 +15,7 @@ public class No448 {
     public static void main(String[] args) {
         int[] nums = new int[] {4,3,2,7,8,2,3,1};
         System.out.println(findDisappearedNumbers(nums));
+//        System.out.println(findDisappearedNumbers2(nums));
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
@@ -26,6 +28,23 @@ public class No448 {
         for (int i = 0; i < n; i++) {
             if(nums[i] <= n) {
                 res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
+        int n = nums.length;
+
+        HashMap<Integer, Integer> ans = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            ans.put(nums[i], ans.getOrDefault(nums[i], 0) + 1);
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        System.out.println(ans);
+        for (int i = 1; i <= n; i++) {
+            if(ans.getOrDefault(i, 0) == 0) {
+                res.add(i);
             }
         }
         return res;

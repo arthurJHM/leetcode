087fa5db8aede630bebd68d6de83ced438bc2a82
nightsswatch <arthurjihengming@gmail.com>
2021-02-16 15:42:52 +0800

    No406

--- src/com/arthur/leetcode/No406.java
+++ src/com/arthur/leetcode/No406.java
@@ -1,5 +1,10 @@
 package com.arthur.leetcode;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @title: No406
  * @Author ArthurJi
@@ -8,12 +13,49 @@ package com.arthur.leetcode;
  */
 public class No406 {
     public static void main(String[] args) {
-
+        int[][] req= new int[][]{{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        int[][] ans = reconstructQueue(req);
+        for (int[] row : ans) {
+            for (int col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 
 
-    public int[][] reconstructQueue(int[][] people) {
-        return null;
+    public static int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] != o2[0]) {
+                    return o2[0] - o1[0];
+                }
+                else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int row = people.length;
+        int col = people[0].length;
+        int[][] ans = new int[row][col];
+        int num = 0;
+        for (int[] person : people) {
+            if(person[1] > num) {
+                ans[num] =  person;
+            }
+            else {
+                for (int i = num; i > person[1]; i--) {
+                    ans[i] = ans[i - 1];
+                }
+                ans[person[1]] = person;
+            }
+            num++;
+        }
+
+        return ans;
     }
 }
 
@@ -48,3 +90,44 @@ public class No406 {
         0 <= hi <= 106
         0 <= ki < people.length
         题目数据确保队列可以被重建*/
+
+
+/*
+
+https://leetcode-cn.com/problems/queue-reconstruction-by-height/solution/xian-pai-xu-zai-cha-dui-dong-hua-yan-shi-suan-fa-g/
+解题思路
+        题目描述：整数对 (h, k) 表示，其中 h 是这个人的身高，k 是排在这个人前面且身高大于或等于 h 的人数。
+
+        渔（套路）：一般这种数对，还涉及排序的，根据第一个元素正向排序，根据第二个元素反向排序，或者根据第一个元素反向排序，根据第二个元素正向排序，往往能够简化解题过程。
+
+        在本题目中，我首先对数对进行排序，按照数对的元素 1 降序排序，按照数对的元素 2 升序排序。原因是，按照元素 1 进行降序排序，对于每个元素，在其之前的元素的个数，就是大于等于他的元素的数量，而按照第二个元素正向排序，我们希望 k 大的尽量在后面，减少插入操作的次数。
+
+
+        8 / 9
+
+        代码：for 循环实现，额外 res 数组空间存储
+        Python3
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        res = []
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        for p in people:
+            if len(res) <= p[1]:
+                res.append(p)
+            elif len(res) > p[1]:
+                res.insert(p[1], p)
+        return res
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        i = 0
+        while i < len(people):
+            if i > people[i][1]:
+                people.insert(people[i][1], people[i])
+                people.pop(i+1)
+            i += 1
+        return people
+*/

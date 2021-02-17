138723c2758df4309722d195711ed55a550b6439
nightsswatch <arthurjihengming@gmail.com>
2021-02-17 22:58:34 +0800

    No566

--- src/com/arthur/leetcode/No566.java
+++ src/com/arthur/leetcode/No566.java
@@ -13,7 +13,7 @@ import java.util.Queue;
 public class No566 {
     public static void main(String[] args) {
         int[][] nums = new int[][]{{1, 2}, {3, 4}};
-        int[][] ans = new No566().matrixReshape(nums, 1 ,4);
+        int[][] ans = new No566().matrixReshape1(nums, 1 ,4);
     }
 
     public int[][] matrixReshape(int[][] nums, int r, int c) {
@@ -30,6 +30,25 @@ public class No566 {
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 ans[i][j] = temp.poll();
+            }
+        }
+        return ans;
+    }
+
+    public int[][] matrixReshape1(int[][] nums, int r, int c) {
+        int[][] ans = new int[r][c];
+        if(r * c != nums.length * nums[0].length) {
+            return nums;
+        }
+        int row = 0, col = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                ans[row][col] = nums[i][j];
+                col++;
+                if(col == c) {
+                    col = 0;
+                    row += 1;
+                }
             }
         }
         return ans;

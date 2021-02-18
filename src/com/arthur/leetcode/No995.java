87e184cd1b508dbcf44ccac7da24d624fb480fac
nightsswatch <arthurjihengming@gmail.com>
2021-02-18 12:26:50 +0800

    No566

---
+++ src/com/arthur/leetcode/No995.java
@@ -0,0 +1,13 @@
+package com.arthur.leetcode;
+
+/**
+ * @title: No995
+ * @Author ArthurJi
+ * @Date: 2021/2/18 12:26
+ * @Version 1.0
+ */
+public class No995 {
+    public static void main(String[] args) {
+
+    }
+}

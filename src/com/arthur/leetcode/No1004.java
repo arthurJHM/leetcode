1659e1d2a30d89a1b4cf9154bba74bf4152b9014
nightsswatch <arthurjihengming@gmail.com>
2021-02-19 00:02:33 +0800

    No1004

---
+++ src/com/arthur/leetcode/No1004.java
@@ -0,0 +1,16 @@
+package com.arthur.leetcode;
+
+/**
+ * @title: No1004
+ * @Author ArthurJi
+ * @Date: 2021/2/19 0:01
+ * @Version 1.0
+ */
+public class No1004 {
+    public static void main(String[] args) {
+
+    }
+    public int longestOnes(int[] A, int K) {
+
+    }
+}

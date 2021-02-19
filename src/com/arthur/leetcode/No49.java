64140ecc6371313f798c41b99ce2d92cad757f23
nightsswatch <arthurjihengming@gmail.com>
2021-02-19 14:53:26 +0800

    No49

---
+++ src/com/arthur/leetcode/No49.java
@@ -0,0 +1,61 @@
+package com.arthur.leetcode;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @title: No49
+ * @Author ArthurJi
+ * @Date: 2021/2/19 14:25
+ * @Version 1.0
+ */
+public class No49 {
+
+    public static void main(String[] args) {
+        List<List<String>> lists = new No49().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        for (List<String> list : lists) {
+            for (String s : list) {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return  new ArrayList<>(Arrays.stream(strs).collect(Collectors.groupingBy(str -> {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            return new String(chars);
+        })).values());
+    }
+
+}
+
+
+/*方法一：排序
+        字母相同，但排列不同的字符串，排序后都一定是相同的。因为每种字母的个数都是相同的，那么排序后的字符串就一定是相同的。
+
+        这里可以利用 stream 的 groupingBy 算子实现直接返回结果：
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return new ArrayList<>(Arrays.stream(strs)
+                .collect(Collectors.groupingBy(str -> {
+                    // 返回 str 排序后的结果。
+                    // 按排序后的结果来grouping by，算子类似于 sql 里的 group by。
+                    char[] array = str.toCharArray();
+                    Arrays.sort(array);
+                    return new String(array);
+                })).values());
+    }
+}
+    注意 groupingBy 算子计算完以后，返回的是一个 Map<String, List<String>>，map 的键是每种排序后的字符串，值是聚合的原始字符串，我们只关心值，所以我们最后 new ArrayList<>(map.values())。
+
+        作者：sweetiee
+        链接：https://leetcode-cn.com/problems/group-anagrams/solution/kan-wo-yi-ju-hua-ac-zi-mu-yi-wei-ci-fen-yrnis/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/

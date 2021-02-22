16d058681423bb47daefd0671e0136f9df14c273
nightsswatch <arthurjihengming@gmail.com>
2021-02-23 01:20:26 +0800

    No207

---
+++ src/com/arthur/leetcode/No207.java
@@ -0,0 +1,17 @@
+package com.arthur.leetcode;
+
+/**
+ * @title: No207
+ * @Author ArthurJi
+ * @Date: 2021/2/22 14:46
+ * @Version 1.0
+ */
+public class No207 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    }
+}

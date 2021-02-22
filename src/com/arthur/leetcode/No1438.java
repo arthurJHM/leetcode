4c62ab57c4eaf5bd69a3aad3b88099fbcfed9f45
nightsswatch <arthurjihengming@gmail.com>
2021-02-22 14:45:20 +0800

    No1438 使用TreeMap use TreeMap

--- src/com/arthur/leetcode/No1438.java
+++ src/com/arthur/leetcode/No1438.java
@@ -2,6 +2,7 @@ package com.arthur.leetcode;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 /**
  * @title: No1438
@@ -11,7 +12,7 @@ import java.util.PriorityQueue;
  */
 public class No1438 {
     public static void main(String[] args) {
-        System.out.println(new No1438().longestSubarray(new int[]{10,1,2,4,7,2}, 5));
+        System.out.println(new No1438().longestSubarray1(new int[]{10,1,2,4,7,2}, 5));
     }
     public int longestSubarray(int[] nums, int limit) {
         int len = nums.length;
@@ -35,6 +36,25 @@ public class No1438 {
                 big = max.peek() - min.peek();
             }
             ans = Math.max(ans, i - right + 1);
+        }
+        return ans;
+    }
+
+    public int longestSubarray1(int[] nums, int limit) {
+        int len = nums.length;
+        int left = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            while(map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if(map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            ans = Math.max(ans, i - left + 1);
         }
         return ans;
     }

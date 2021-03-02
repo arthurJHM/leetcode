5c57c2e842e440c0b512bb0ba1b366ad5063c060
nightsswatch <arthurjihengming@gmail.com>
2021-03-02 10:52:25 +0800

    No303

--- src/com/arthur/leetcode/No303.java
+++ src/com/arthur/leetcode/No303.java
@@ -10,4 +10,17 @@ public class No303 {
     public static void main(String[] args) {
 
     }
+    int preSum[];
+
+
+    public void NumArray(int[] nums) {
+        preSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            preSum[i + 1] = nums[i] + preSum[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return preSum[j+1] - preSum[i];
+    }
 }

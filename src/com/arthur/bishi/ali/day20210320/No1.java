30346d035ff5a17ba07b2553599290ec9f409ae9
nightsswatch <arthurjihengming@gmail.com>
2021-03-20 10:28:30 +0800

    笔试题

--- src/com/arthur/bishi/ali/day20210320/No1.java
+++ src/com/arthur/bishi/ali/day20210320/No1.java
@@ -1,9 +1,9 @@
-package com.arthur.xiecheng;
+package com.arthur.bishi.ali.day20210320;
 
 /**
  * @title: No1
  * @Author ArthurJi
- * @Date: 2021/3/14 15:16
+ * @Date: 2021/3/20 10:26
  * @Version 1.0
  */
 public class No1 {

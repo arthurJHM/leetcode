af4ab699f0a22c2600501ac7462abfb116d77969
nightsswatch <arthurjihengming@gmail.com>
2021-03-22 14:42:48 +0800

    新加入牛客套餐

--- src/com/arthur/nowcoder/NC78.java
+++ src/com/arthur/nowcoder/NC78.java
@@ -7,4 +7,22 @@ package com.arthur.nowcoder;
  * @Version 1.0
  */
 public class NC78 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode Last = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return Last;
+    }
 }

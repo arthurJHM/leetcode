1d7126c75c497834077d73936afba24e358ca196
nightsswatch <arthurjihengming@gmail.com>
2021-04-05 11:30:46 +0800

    不想写kmp 再见

---
+++ src/com/arthur/leetcode/No28.java
@@ -0,0 +1,17 @@
+package com.arthur.leetcode;
+
+/**
+ * @title: No28
+ * @Author ArthurJi
+ * @Date: 2021/4/5 10:30
+ * @Version 1.0
+ */
+public class No28 {
+    public static void main(String[] args) {
+
+    }
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}

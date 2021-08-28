7c9cec58e935fe865d38dee8d3343258710eb06b
arthurJHM <arthurjihengming@gmail.com>
2021-08-28 17:52:57 +0800

    zoom

--- src/com/arthur/bishi/zoom0828/No2.java
+++ src/com/arthur/bishi/zoom0828/No2.java
@@ -1,5 +1,7 @@
 package com.arthur.bishi.zoom0828;
 
+import java.util.ArrayDeque;
+
 /**
  * @description：
  * @author： arthurji
@@ -8,4 +10,19 @@ package com.arthur.bishi.zoom0828;
  * @version: 1.0
  */
 public class No2 {
+    public int find132Pattern (int[] nums) {
+        // write code here
+        int n = nums.length;
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        int k = Integer.MIN_VALUE;
+        int ans = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(nums[i] < k) ans++;
+            while (!d.isEmpty() && d.peekLast() < nums[i]) {
+                k = Math.max(k, d.pollLast());
+            }
+            d.addLast(nums[i]);
+        }
+        return ans;
+    }
 }
--- src/com/arthur/bishi/zoom0828/No3.java
+++ src/com/arthur/bishi/zoom0828/No3.java
@@ -1,5 +1,8 @@
 package com.arthur.bishi.zoom0828;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 /**
  * @description：
  * @author： arthurji
@@ -8,4 +11,24 @@ package com.arthur.bishi.zoom0828;
  * @version: 1.0
  */
 public class No3 {
+    public int[] slideWindow (int[] nums, int windowSize, int step) {
+        int n = nums.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
+        while (i < n) {
+            int j = i + windowSize;
+            j = Integer.min(j, n);
+            int temp = Integer.MIN_VALUE;
+            for (int k = i; k < j; k++) {
+                temp = Integer.max(temp, nums[k]);
+            }
+            ans.add(temp);
+            i = i + step;
+        }
+        int[] re = new int[ans.size()];
+        for (int k = 0; k < re.length; k++) {
+            re[k] = ans.get(k);
+        }
+        return re;
+    }
 }

b44734cbb2b7c5a8f9987539eb003678a7fa08b2
nightsswatch <arthurjihengming@gmail.com>
2021-09-22 21:13:50 +0800

    No647

---
+++ src/com/arthur/bishi/test/Test1.java
@@ -0,0 +1,15 @@
+package com.arthur.bishi.test;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @title: Test1
+ * @Author hengmingji
+ * @Date: 2021/9/18 12:56
+ * @Version 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("what are your abcdefghijklmno    what your favorite food??");
+    }
+}
